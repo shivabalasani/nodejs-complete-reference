d81032502bea910a77550f7d72b78e276c92844f
Balasani <Shiva.Balasani@CSLPLASMA.COM>
2019-12-18 15:41:49 -0500

    Updated access tokens

--- web-server/src/utils/geocode.js
+++ web-server/src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('request')
 
 const geocode = (address, callback) => {
-    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1IjoiYW5kcmV3bWVhZDEiLCJhIjoiY2pvOG8ybW90MDFhazNxcnJ4OTYydzJlOSJ9.njY7HvaalLEVhEOIghPTlw&limit=1'
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic2JhbGFzYW5pIiwiYSI6ImNrNDh3MGcxZzFhY2MzZ28xeG9rcDZ0OXAifQ.ec37UrfehkmL3LadUSQtpw&limit=1'
 
     request({ url, json: true }, (error, { body }) => {
         if (error) {
